80e51632544c54cd379365c074f8099c8eaa0150
jyeros <jyero96@gmail.com>
2020-08-31 04:52:41 -0400

    Add migration project and first migration

---
+++ src/CDSHooks.Migrations/Migrations/20200831081248_CreateHookSchema.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CDSHooks.Migrations.Migrations
+{
+    public partial class CreateHookSchema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Hook",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Workflow = table.Column<string>(nullable: true),
+                    Context = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hook", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Hook");
+        }
+    }
+}
