ef45c463e15b29b25b030edd9fb70a6f3226b717
jyeros <jyero96@gmail.com>
2020-09-02 05:22:55 -0400

    Add prefetch token builder

---
+++ src/CDSHooks.Core/PrefetchTemplate/PrefetchTokenBuilder.cs
@@ -0,0 +1,17 @@
+﻿using CDSHooks.Domain;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CDSHooks.Core.PrefetchTemplate
+{
+    public static class PrefetchTokenBuilder
+    {
+        public static IDictionary<string, object> Build(Dictionary<string, HookContext> hookContext,
+            IDictionary<string, object> context)
+        {
+            return context.Where(x => hookContext.TryGetValue(x.Key, out var contextDefinition)
+                        && contextDefinition.IsPrefetchToken)
+                .ToDictionary(x => $"context.{x.Key}", x => x.Value);
+        }
+    }
+}
